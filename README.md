5bb93958fb5df29de3abcf54ad2950c2bb6d6d14
huabin <535675+huabin@users.noreply.github.com>
2024-02-06 16:47:00 +0800

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# Warning
+
+This is a demo for a [post](create-react-a-component-and-publish-it-on-npmjs) on my blog.
+
+Don't use!!!
+
+Don't use!!!
+
+Don't use!!!
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss'
+
+const Alert = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>React + Sass</p>
+    </div>
+  )
+}
+export default Alert;
